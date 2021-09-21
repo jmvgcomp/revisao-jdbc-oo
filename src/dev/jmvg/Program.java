0da356d5469a2b4300f9394188abf465a8c47c06
Joanantha Matheus <jmvg.comp@gmail.com>
2021-09-21 01:30:36 -0300

    Pedido com produtos

--- src/dev/jmvg/Program.java
+++ src/dev/jmvg/Program.java
@@ -9,6 +9,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Program {
     public static void main(String[] args) throws SQLException {
@@ -16,23 +18,44 @@ public class Program {
         Connection conn = DBConnection.getConnection();
         Statement st = conn.createStatement();
 
-        ResultSet rs = st.executeQuery("SELECT  * FROM tb_order");
+        ResultSet rs = st.executeQuery("SELECT * FROM tb_order " +
+                "INNER JOIN tb_order_product ON tb_order.id = tb_order_product.order_id " +
+                "INNER JOIN tb_product ON tb_product.id = tb_order_product.product_id");
+
+
+        Map<Long, Order> map = new HashMap<>();
+        Map<Long, Product> prods = new HashMap<>();
 
         while (rs.next()){
+            Long orderId = rs.getLong("order_id");
+            if(map.get(orderId) == null){
+                map.put(orderId, instantiateOrder(rs));
+            }
 
+            Long productId = rs.getLong("product_id");
+            if(prods.get(productId) == null){
+                prods.put(productId, instantiateProduct(rs));
+            }
 
+            map.get(orderId).getProducts().add(prods.get(productId));
 
-            System.out.println(instantiateOrder(rs));
 
         }
 
+        for (Long orderId : map.keySet()){
+            System.out.println(map.get(orderId));
+            for (Product p: map.get(orderId).getProducts()) {
+                System.out.println(p);
+            }
+            System.out.println();
+        }
 
     }
 
     private static Product instantiateProduct(ResultSet rs) throws SQLException {
         Product p = new Product();
 
-        p.setId(rs.getLong("Id"));
+        p.setId(rs.getLong("product_id"));
         p.setDescription(rs.getString("description"));
         p.setImageUrl(rs.getString("image_uri"));
         p.setName(rs.getString("name"));
@@ -44,7 +67,7 @@ public class Program {
     private static Order instantiateOrder(ResultSet rs) throws SQLException{
         Order order = new Order();
 
-        order.setId(rs.getLong("id"));
+        order.setId(rs.getLong("order_id"));
         order.setLatitude(rs.getDouble("latitude"));
         order.setLongitude(rs.getDouble("longitude"));
         order.setMoment(rs.getTimestamp("moment").toInstant());
